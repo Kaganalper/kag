7eaede75003c2c85cc913b6fe417087c93892d7e
Kagan Alper <kagancin@outlook.com>
2013-02-27 21:36:05 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veryfirst::Application.config.secret_token = '8e27cbca5db0a7787e09ed9a80187846bd632baf9719f9a9cca503ba0ef3fabce397dbaf51f6de16699120c001a91d0866f71c21fae6db9d74382b234d05f8f8'
